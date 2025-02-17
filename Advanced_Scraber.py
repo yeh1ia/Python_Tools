2af3aacb2f06339b52bd5fc53ca8976a9cbace82
yeh1ia <132021753+yeh1ia@users.noreply.github.com>
2025-02-17 18:58:23 +0200

    Add files via upload

---
+++ Advanced_Scraber.py
@@ -0,0 +1,47 @@
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import requests
+import validators
+
+Visited_urls = set() # to put uniq links
+
+def scrab(url , keyword):
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Failed To Connect With Your Target {url}")
+        return
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        #print(soup)
+
+        items = [] # to put links and non links then validate them.
+
+        a_tag = soup.find_all("a")
+        link_tag = soup.find_all("link")
+        for tag in link_tag:
+            href = tag.get("href")
+            if href is not None and href != '':
+                items.append(href)
+        for tag in a_tag:
+            href2 = tag.get("href")
+            if href2 is not None and href2 != '':
+                items.append(href2)
+        valid_links = [item for item in items if validators.url(item)]
+        #print(valid_links)
+
+        for urls2 in valid_links:
+            if urls2 not in Visited_urls:
+                Visited_urls.add(urls2)
+                url_join = urljoin(url , urls2)
+                if keyword in url_join:
+                    print(url_join)
+                    scrab(url_join,keyword) # We call function again to deep recursive pages
+            else:
+                pass
+
+
+
+url = input("Enter Your Target: ")
+keyword = input("Enter Your Keyword: ")
+scrab(url,keyword)
